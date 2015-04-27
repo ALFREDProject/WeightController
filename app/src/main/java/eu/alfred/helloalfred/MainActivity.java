806c495b4948c111130b87100939d12cdfb49928
Daniel Gilbert <gilbert@ascora.de>
2015-04-27 14:59:44 +0200

    Initial Commit

---
+++ app/src/main/java/eu/alfred/helloalfred/MainActivity.java
@@ -0,0 +1,171 @@
+package eu.alfred.helloalfred;
+
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import eu.alfred.api.PersonalAssistant;
+import eu.alfred.api.PersonalAssistantConnection;
+import eu.alfred.api.storage.CloudStorage;
+import eu.alfred.api.storage.responses.BucketResponse;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private PersonalAssistant personalAssistant;
+    private CloudStorage cloudStorage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        cloudStorage = null;
+        personalAssistant = new PersonalAssistant(this);
+
+        personalAssistant.setOnPersonalAssistantConnectionListener(new PersonalAssistantConnection() {
+            @Override
+            public void OnConnected() {
+                // Do some stuff
+                cloudStorage = new CloudStorage(personalAssistant.getMessenger());
+                sendNotification("Connected to AlfredService");
+            }
+
+            @Override
+            public void OnDisconnected() {
+                // Do some cleanup stuff
+            }
+        });
+
+        personalAssistant.Init();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onBtnSaveClick(View view) {
+        EditText nameText = (EditText)this.findViewById(R.id.nameText);
+        updateText(nameText.getText().toString());
+    }
+
+    private void updateText(String text) {
+
+        JSONObject queryObj = new JSONObject();
+        try {
+            queryObj.put("key", "HelloAlfredName");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("name", text);
+            obj.put("key", "HelloAlfredName");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        cloudStorage.saveJsonObject("HelloAlfredStructuredBucket", obj, new BucketResponse() {
+            @Override
+            public void OnError(Exception e) {
+
+            }
+
+            @Override
+            public void OnSuccess(JSONObject jsonObject) {
+
+            }
+        });
+    }
+
+    //region NotificationHelper
+    private void sendNotification(String notificationDetails) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Define the notification settings.
+        builder.setSmallIcon(R.drawable.ic_launcher)
+                // In a real app, you may want to use a library like Volley
+                // to decode the Bitmap.
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                        R.drawable.ic_launcher))
+                .setColor(Color.BLUE)
+                .setContentTitle(notificationDetails)
+                .setContentText("Notification")
+                .setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+    //endregion
+
+}
